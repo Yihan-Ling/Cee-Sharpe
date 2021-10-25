7a5a35cc0ad6b6fa6a855dc1ddb3a7359d427799
Carson Ling <you@example.com>
2021-10-25 12:20:40 -0400

    Added FirstCSharpeCode Framework

---
+++ FirstCSharpeCode/FirstCSharpeCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCSharpeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
